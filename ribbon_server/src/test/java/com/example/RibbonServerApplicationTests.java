b27a9110afbacb4e772d3fe8463f8811a920c869
chuchen <xzhmuma@qq.com>
2017-04-08 16:15:54 +0800

    初始版

--- ribbon_server/src/test/java/com/example/RibbonServerApplicationTests.java
+++ ribbon_server/src/test/java/com/example/RibbonServerApplicationTests.java
@@ -2,6 +2,8 @@ package com.example;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 
 @RunWith(SpringRunner.class)
