820dd368b474856650546429ac00856cfe8ffef0
chuchen <xzhmuma@qq.com>
2017-04-17 20:25:58 +0800

    初始版

--- feign_server/src/main/java/com/example/FeignServerApplication.java
+++ feign_server/src/main/java/com/example/FeignServerApplication.java
@@ -1,18 +1,20 @@
 package com.example;
 
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 
 @EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
+@EnableHystrixDashboard
 public class FeignServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FeignServerApplication.class, args);
+        new SpringApplicationBuilder(FeignServerApplication.class).web(true).run(args);
     }
 }
